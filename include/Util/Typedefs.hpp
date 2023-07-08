80377ba3acde45bec7d945ac473da03c35e8d2bf
Markus Björkberger <m.bjorkberger@gmail.com>
2023-07-08 10:18:12 +0200

    format

--- include/Util/Typedefs.hpp
+++ include/Util/Typedefs.hpp
@@ -1,32 +1,32 @@
 #pragma once
 #include <cstdint>
 
-typedef int_fast32_t  i32;
+typedef int_fast32_t i32;
 
-typedef int_fast64_t  i64;
+typedef int_fast64_t i64;
 
 typedef uint_fast32_t u32;
 
 typedef uint_fast64_t u64;
 
-typedef float         f32;
+typedef float f32;
 
-typedef double        f64;
+typedef double f64;
 
-typedef bool          b;
+typedef bool b;
 
-typedef int8_t        i8_t;
+typedef int8_t i8_t;
 
-typedef int16_t       i16_t;
+typedef int16_t i16_t;
 
-typedef int32_t       i32_t;
+typedef int32_t i32_t;
 
-typedef int64_t       i64_t;
+typedef int64_t i64_t;
 
-typedef uint8_t       u8_t;
+typedef uint8_t u8_t;
 
-typedef uint16_t      u16_t;
+typedef uint16_t u16_t;
 
-typedef uint32_t      u32_t;
+typedef uint32_t u32_t;
 
-typedef uint64_t      u64_t;
+typedef uint64_t u64_t;
